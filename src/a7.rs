802261b05b71bef7864a85d5f911e4ee174ed0f8
Jan Češpivo <jan.cespivo@gmail.com>
2024-01-06 10:53:14 +0100

    day 7 part 2

--- src/a7.rs
+++ src/a7.rs
@@ -1,5 +1,4 @@
 use std::fs::read_to_string;
-use counter::Counter;
 use itertools::Itertools;
 
 const CARDS: &str = "23456789TJQKA";
@@ -12,12 +11,12 @@ fn parse(input: &str) -> Vec<(Vec<usize>, Vec<usize>, usize)> {
     for line in input.lines() {
         let (cards_str, bid_str) = line.split_once(" ").unwrap();
         let mut hand: Vec<usize> = vec![];
-        let mut part_2_ranks: Vec<usize> = vec![];
+        let mut part_2_hand: Vec<usize> = vec![];
         for card in cards_str.chars() {
             hand.push(CARDS.find(card).unwrap());
-            part_2_ranks.push(PART_2_CARDS.find(card).unwrap())
+            part_2_hand.push(PART_2_CARDS.find(card).unwrap())
         }
-        hands.push((part_2_ranks, hand, bid_str.parse().unwrap()))
+        hands.push((part_2_hand, hand, bid_str.parse().unwrap()))
     }
     hands
 
@@ -33,48 +32,61 @@ fn parse(input: &str) -> Vec<(Vec<usize>, Vec<usize>, usize)> {
 fn get_kind(hand: &Vec<usize>) -> usize {
     let binding = hand
         .iter()
-        .collect::<Counter<_>>()
-        .most_common_ordered();
-    if binding.len() == 1 {
-        return 6;
-    }
-    let kind_counts = binding[0..=1]
-        .iter()
-        .map(|(_, count)| count)
-        .collect_tuple().unwrap();
+        .counts();
 
-    match kind_counts {
-        (&4_usize, _) => 5,
-        (&3_usize, &2_usize) => 4,
-        (&3_usize, _) => 3,
-        (&2_usize, &2_usize) => 2,
-        (&2_usize, _) => 1,
+    let mut kinds: Vec<_> = binding.iter().map(|(_card, count)| count).collect();
+    kinds.sort();
+    kinds.reverse();
+
+    match kinds[0] {
+        5 => 6,
+        4 => 5,
+        3 => match kinds[1] {
+            2 => 4,
+            _ => 3,
+        },
+        2 => match kinds[1] {
+            2 => 2,
+            _ => 1,
+        }
         _ => 0,
     }
 }
 
 fn get_part_2_kind(hand: &Vec<usize>) -> usize {
-    // let binding = hand
-    //     .iter()
-    //     .collect::<Counter<_>>()
-    //     .most_common_ordered();
-    // if binding.len() == 1 {
-    //     return 6;
-    // }
-    // let kind_counts = binding[0..=1]
-    //     .iter()
-    //     .map(|(_, count)| count)
-    //     .collect_tuple().unwrap();
-    //
-    // base_match kind_counts {
-    //     (&4_usize, _) => 5,
-    //     (&3_usize, &2_usize) => 4,
-    //     (&3_usize, _) => 3,
-    //     (&2_usize, &2_usize) => 2,
-    //     (&2_usize, _) => 1,
-    //     _ => 0,
-    // }
-    0
+    let mut binding = hand
+        .iter()
+        .counts();
+
+    let jokers_count = binding.remove(&0).unwrap_or(0);
+
+    let mut kinds: Vec<_> = binding
+        .iter()
+        .map(|(_card, count)| *count)
+        .collect();
+
+    kinds.sort();
+    kinds.reverse();
+
+    let first_kind = kinds.get(0).unwrap_or(&0_usize) + jokers_count;
+
+    if first_kind == 5 {
+        6
+    } else if first_kind == 4  {
+        5
+    } else if first_kind == 3 {
+        match kinds[1] {
+            2 => 4,
+            _ => 3,
+        }
+    } else if first_kind == 2 {
+        match kinds[1] {
+            2 => 2,
+            _ => 1,
+        }
+    } else {
+        0
+    }
 }
 
 fn part_1(input: &str) -> usize {
@@ -88,22 +100,22 @@ fn part_1(input: &str) -> usize {
     hands_ranked
         .iter()
         .enumerate()
-        .map(|(index, (_kind, _hand, bid))| (index + 1) * *bid)
+        .map(|(index, (_kind, _hand, bid))| (index + 1) * **bid)
         .sum()
 }
 
-fn part_2(input: &str) -> u64 {
+fn part_2(input: &str) -> usize {
     let hands = parse(input);
 
     let mut hands_ranked: Vec<(usize, &Vec<usize>, &usize)> = hands.iter().map(
-        |(part_2_ranks, hand, bid)|
-            (get_part_2_kind(hand), part_2_ranks, bid)
+        |(part_2_hand, _, bid)|
+            (get_part_2_kind(part_2_hand), part_2_hand, bid)
     ).collect();
     hands_ranked.sort();
     hands_ranked
         .iter()
         .enumerate()
-        .map(|(index, (_kind, _part_2_ranks, bid))| (index + 1) * *bid)
+        .map(|(index, (_kind, _part_2_hand, bid))| (index + 1) * *bid)
         .sum()
 }
 
@@ -129,11 +141,11 @@ QQQJA 483";
         assert_eq!(
             parse(INPUT),
             vec![
-                (vec![1, 0, 8, 1, 11], 765),
-                (vec![8, 3, 3, 9, 3], 684),
-                (vec![11, 11, 4, 5, 5], 28),
-                (vec![11, 8, 9, 9, 8], 220),
-                (vec![10, 10, 10, 9, 12], 483),
+                (vec![2, 1, 9, 2, 11], vec![1, 0, 8, 1, 11], 765),
+                (vec![9, 4, 4, 0, 4], vec![8, 3, 3, 9, 3], 684),
+                (vec![11, 11, 5, 6, 6], vec![11, 11, 4, 5, 5], 28),
+                (vec![11, 9, 0, 0, 9], vec![11, 8, 9, 9, 8], 220),
+                (vec![10, 10, 10, 0, 12], vec![10, 10, 10, 9, 12], 483),
             ]);
     }
 
